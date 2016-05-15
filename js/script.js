b60b1cb2f88301b4d169114e1d461ff66004d202
Michael Toback <mtoback@vmware.com>
2016-05-15 11:55:19 -0700

    refactored to correct mvc errors

--- js/script.js
+++ js/script.js
@@ -15,25 +15,15 @@ var view = {
 /** generate the list of cats from the names, appearing on the left side */
 init: function (){
   $("#cat-list").append('<ul style="list-style: none"  id="cats">');
-  for (var index = 0; index < model.cats.length; index++) {
-    $('#cats').append('<li id="cat-name'+index + '"class="name">' +
-    model.cats[index] + '</li>');
-   }
+  controller.initializeList();
   /** now that we have the list initialize the image from it **/
   $('#image').html('<div><span>' + model.cats[model.currentCat] + '</span>: <span id="clicks">' + model.clicks[model. currentCat] + '</span></div>' +
     	'<img src="img/cat' + model.currentCat + '.jpg" height=400>');
 
   },
   render: function(){
-    update_image();
-    update_clicks()
-  },
-  update_image: function(){
-     $('#image').html('<div><span>' + model.cats[model.currentCat] + '</span>:   <span id="clicks">' + model.clicks[model.currentCat] + '</span></div>' +
-      '<img src="img/cat' + model.currentCat + '.jpg" height=400>');
-  },
-  update_clicks: function(currentCat,clicks){
-    $('#clicks').text(model.clicks[model.currentCat]);
+    controller.update_image();
+    controller.update_clicks()
   }
 }
 
@@ -50,15 +40,28 @@ var controller = {
       var idval = id.slice(8); // need to start from 8th element because form of id is 'cat-namex' where x is a one up number
       var idnum = parseInt(idval);
       model.currentCat = idnum;
-      view.update_image();
+      controller.update_image();
     });
 
     // when clicking on image div update the click count
     $('#image').click(function(){
       model.clicks[model.currentCat] += 1;
-      view.update_clicks();
+      controller.update_clicks();
     });
     view.init();
+  },
+  initializeList: function(){
+  for (var index = 0; index < model.cats.length; index++) {
+    $('#cats').append('<li id="cat-name'+index + '"class="name">' +
+    model.cats[index] + '</li>');
+   }
+  },
+  update_image: function(){
+     $('#image').html('<div><span>' + model.cats[model.currentCat] + '</span>:   <span id="clicks">' + model.clicks[model.currentCat] + '</span></div>' +
+      '<img src="img/cat' + model.currentCat + '.jpg" height=400>');
+  },
+  update_clicks: function(currentCat,clicks){
+    $('#clicks').text(model.clicks[model.currentCat]);
   }
 };
 controller.init();
